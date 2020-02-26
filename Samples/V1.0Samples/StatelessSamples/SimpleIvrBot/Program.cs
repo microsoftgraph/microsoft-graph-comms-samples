5a837bf60e8af71af8550278acb55fb413fbe426
Matthew Howard <mathowar@microsoft.com>
2020-02-26 14:32:20 -0800

    update sample docs

---
+++ Samples/V1.0Samples/StatelessSamples/SimpleIvrBot/Program.cs
@@ -0,0 +1,42 @@
+﻿// <copyright file="Program.cs" company="Microsoft Corporation">
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+// </copyright>
+
+namespace SimpleIvrBot
+{
+    using Microsoft.AspNetCore;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.Logging;
+
+    /// <summary>
+    /// The program class.
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// The main function.
+        /// </summary>
+        /// <param name="args">Array of arguments.</param>
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        /// <summary>
+        /// Create web host builder.
+        /// </summary>
+        /// <param name="args">arguments.</param>
+        /// <returns>Returns WebHostBuilder.</returns>
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .ConfigureLogging((hostingContext, logging) =>
+                {
+                    logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+                    logging.AddDebug();
+                    logging.AddConsole();
+                    logging.AddAzureWebAppDiagnostics();
+                })
+                .UseStartup<Startup>();
+    }
+}
