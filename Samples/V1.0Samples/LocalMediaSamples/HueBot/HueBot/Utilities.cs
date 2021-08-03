c39f769a860d41c73c3e9fe92755b73a0f3e3822
Dominik Messinger <domessin@microsoft.com>
2021-08-03 10:52:10 -0700

    First code to correctly resize, place and clip a YUV image

--- Samples/V1.0Samples/LocalMediaSamples/HueBot/HueBot/Utilities.cs
+++ Samples/V1.0Samples/LocalMediaSamples/HueBot/HueBot/Utilities.cs
@@ -8,8 +8,11 @@
 // </summary>
 // --------------------------------------------------------------------------------------------------------------------
 
+#pragma warning disable
 namespace Sample.HueBot
 {
+    using System;
+    using System.Drawing;
     using System.Runtime.InteropServices;
     using Microsoft.Skype.Bots.Media;
     using Sample.HueBot.Bot;
@@ -99,7 +102,20 @@ namespace Sample.HueBot
         {
             byte[] buffer = new byte[videoMediaBuffer.VideoFormat.Width * videoMediaBuffer.VideoFormat.Height * 12 / 8];
             Marshal.Copy(videoMediaBuffer.Data, buffer, 0, buffer.Length);
-            ApplyHue(buffer, hueColor, videoMediaBuffer.VideoFormat.Width, videoMediaBuffer.VideoFormat.Height);
+
+            // byte[] buffer = CreateGreenYUV(videoMediaBuffer.VideoFormat.Width, videoMediaBuffer.VideoFormat.Height);
+            // ApplyHue(buffer, hueColor, videoMediaBuffer.VideoFormat.Width, videoMediaBuffer.VideoFormat.Height);
+            // DoubleFrame(buffer, videoMediaBuffer.VideoFormat.Width, videoMediaBuffer.VideoFormat.Height);
+
+            var yuv = new YUV(buffer, videoMediaBuffer.VideoFormat.Width, videoMediaBuffer.VideoFormat.Height, videoMediaBuffer.Stride);
+
+            //if (DateTime.Now.Second < 30)
+            //    PictureOnMono(yuv, 3);
+            //else
+            //    PictureInPicture(yuv, 3);
+
+            PresenterView(yuv);
+
             return buffer;
         }
 
@@ -146,6 +162,195 @@ namespace Sample.HueBot
         private static void AddWithoutRollover(byte[] buffer, int index, int value)
         {
             buffer[index] = (byte)(value + buffer[index]).Clamp(0, byte.MaxValue);
+        }
+
+        /// <summary>
+        /// Double.
+        /// </summary>
+        private static void DoubleFrame(byte[] buffer, int width, int height)
+        {
+            int cut = width / 4;
+            for (var x = 0; x < width / 2; x += 1)
+            {
+                for (var y = 0; y < height; y += 1)
+                {
+                    var i = (width * y) + x;
+                    buffer[i] = buffer[i + cut];
+                    buffer[i + (width / 2)] = buffer[i + cut];
+                }
+            }
+        }
+
+        private static void SolidFill(YUV input, byte red, byte green, byte blue)
+        {
+            var (yValue, uValue, vValue) = RGBtoYUV(red, green, blue);
+
+            for (var y = 0; y < input.Height; y++)
+            {
+                for (var x = 0; x < input.Width; x++)
+                {
+                    input.Y[x, y] = yValue;
+                    input.U[x, y] = uValue;
+                    input.V[x, y] = vValue;
+                }
+            }
+        }
+
+        private static (byte y, byte u, byte v) RGBtoYUV(byte r, byte g, byte b)
+        {
+            unchecked
+            {
+                var y = (byte)(((66 * r + 129 * g + 25 * b + 128) >> 8) + 16);
+                var u = (byte)(((-38 * r - 74 * g + 112 * b + 128) >> 8) + 128);
+                var v = (byte)(((112 * r - 94 * g - 18 * b + 128) >> 8) + 128);
+
+                return (y, u, v);
+            }
+        }
+
+        private static void PictureOnMono(YUV input, int scale)
+        {
+            var resized = ResizeYUV(input, input.Width / scale, input.Height / scale);
+            SolidFill(input, 128, 128, 128);
+            PlaceAt(input, resized, new Point(-100, 0));
+            PlaceAt(input, resized, new Point(input.Width / 2, input.Height / 2));
+        }
+
+        private static void PictureInPicture(YUV input, int scale)
+        {
+            var pictureWidth = input.Width / scale;
+            var pictureHeight = input.Height / scale;
+            var resized = ResizeYUV(input, pictureWidth, pictureHeight);
+            PlaceAt(input, resized, new Point(-100, 0));
+            PlaceAt(input, resized, new Point(input.Width / 2, input.Height / 2));
+        }
+
+        /*
+        1111100000000000000000000000000000001111
+        1111100000000000000000000000000000001111
+        -----00000000000000000000000000000001111
+        1111100000000000000000000000000000000000
+        1111100000000000000000000000000000000000
+        -----00000000000000000000000000000000000
+        1111100000000000000000000000000000000000
+        1111100000000000000000000000000000000000
+        -----00000000000000000000000000000000000
+        1111100000000000000000000000000000000000
+        1111100000000000000000000000000000000000
+         */
+        private static void PresenterView(YUV input)
+        {
+            var audienceCount = 6;
+            var borderWidth = input.Width / 20;
+
+            var audience = ResizeYUV(input, input.Width / audienceCount, input.Height / audienceCount);
+            for (var i = 0; i < audienceCount; i++)
+                PlaceAt(input, audience, new Point(0, i * audience.Height));
+
+            var presenterScale = 4;
+            var presenter = ResizeYUV(input, input.Width / presenterScale, input.Height / presenterScale);
+            PlaceAt(
+                input,
+                presenter,
+                new Point(input.Width - presenter.Height - borderWidth, borderWidth),
+                new Rectangle((presenter.Width - presenter.Height) / 2, 0, presenter.Height, presenter.Height));
+        }
+
+        private static YUV ResizeYUV(YUV original, int resizedWidth, int resizedHeight)
+        {
+            var resized = new YUV(resizedWidth, resizedHeight, original.Stride);
+
+            var heightConversionRatio = original.Height * 1.0 / resizedHeight;
+            var widthConversionRatio = original.Width * 1.0 / resizedWidth;
+
+            SetY();
+            SetUV();
+
+            return resized;
+
+            void SetY()
+            {
+                for (var y = 0; y < resizedHeight; y++)
+                {
+                    for (var x = 0; x < resizedWidth; x++)
+                    {
+                        var originalX = (int)(x * heightConversionRatio);
+                        var originalY = (int)(y * widthConversionRatio);
+                        try
+                        {
+                            resized.Y[x, y] = original.Y[originalX, originalY];
+                        }
+                        catch
+                        {
+                        }
+                    }
+                }
+            }
+
+            void SetUV()
+            {
+                for (var y = 0; y < resized.Height; y += 2)
+                {
+                    for (var x = 0; x < resized.Width; x += 2)
+                    {
+                        int originalX = (int)(x * widthConversionRatio);
+                        int originalY = (int)(y * heightConversionRatio);
+                        try
+                        {
+                            resized.U[x, y] = original.U[originalX, originalY];
+                            resized.V[x, y] = original.V[originalX, originalY];
+                        }
+                        catch
+                        {
+
+                        }
+                    }
+                }
+            }
+        }
+
+        private static void PlaceAt(YUV input, YUV picture, Point offset, Rectangle? clip = null)
+        {
+            var clipRectangle = clip ?? new Rectangle(0, 0, picture.Width, picture.Height);
+            clipRectangle = new Rectangle(
+                Math.Min(Math.Max(0, clipRectangle.X), picture.Width),
+                Math.Min(Math.Max(0, clipRectangle.Y), picture.Height),
+                Math.Max(0, Math.Min(picture.Width, clipRectangle.Width)),
+                Math.Max(0, Math.Min(picture.Height, clipRectangle.Height)));
+
+            SetY();
+            SetUV();
+
+            void SetY()
+            {
+                for (var y = clipRectangle.Top; y < clipRectangle.Bottom; y++)
+                {
+                    for (var x = clipRectangle.Left; x < clipRectangle.Right; x++)
+                    {
+                        var targetX = x - clipRectangle.Left + offset.X;
+                        var targetY = y - clipRectangle.Top + offset.Y;
+                        if (targetX < 0 || targetX >= input.Width || targetY < 0 || targetY >= input.Height)
+                            continue;
+                        input.Y[targetX, targetY] = picture.Y[x, y];
+                    }
+                }
+            }
+
+            void SetUV()
+            {
+                for (var y = clipRectangle.Top; y < clipRectangle.Bottom; y += 2)
+                {
+                    for (var x = clipRectangle.Left; x < clipRectangle.Right; x += 2)
+                    {
+                        var targetX = x - clipRectangle.Left + offset.X ;
+                        var targetY = y - clipRectangle.Top + offset.Y;
+                        if (targetX < 0 || targetX >= input.Width || targetY < 0 || targetY >= input.Height)
+                            continue;
+                        input.U[targetX, targetY] = picture.U[x, y];
+                        input.V[targetX, targetY] = picture.V[x, y];
+                    }
+                }
+            }
         }
     }
 }
---
+++ Samples/V1.0Samples/LocalMediaSamples/HueBot/HueBot/YUV.cs
@@ -0,0 +1,103 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="YUV.cs" company="Microsoft Corporation">
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+// </copyright>
+// <summary>
+//   The utilities class.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+#pragma warning disable
+namespace Sample.HueBot
+{
+    using System.Runtime.InteropServices;
+    using Microsoft.Skype.Bots.Media;
+    using Sample.HueBot.Bot;
+
+	public class YUV
+	{
+		public readonly byte[] Buffer;
+		public readonly int Width;
+		public readonly int Height;
+		public readonly int Stride;
+		public readonly YBuffer Y;
+		public readonly UBuffer U;
+		public readonly VBuffer V;
+
+		public YUV(int width, int height, int stride)
+			: this(new byte[stride * height * 3 / 2], width, height, stride)
+		{ }
+
+		public YUV(byte[] buffer, int width, int height, int stride)
+		{
+			Buffer = buffer;
+			Width = width;
+			Height = height;
+			Stride = stride;
+
+			Y = new YBuffer(this);
+			U = new UBuffer(this);
+			V = new VBuffer(this);
+		}
+
+		public class YBuffer
+		{
+			private readonly YUV _yuv;
+
+			public YBuffer(YUV yuv)
+				=> _yuv = yuv;
+
+			private int Index(int x, int y)
+				=> y * _yuv.Stride + x;
+
+			public byte this[int x, int y]
+			{
+				get => _yuv.Buffer[Index(x, y)];
+				set => _yuv.Buffer[Index(x, y)] = value;
+			}
+		}
+
+		public class UBuffer
+		{
+			private readonly YUV _yuv;
+			private readonly int _start;
+
+			public UBuffer(YUV yuv)
+			{
+				_yuv = yuv;
+				_start = yuv.Stride * yuv.Height;
+			}
+
+			private int Index(int x, int y)
+				=> _start + (y / 2 * _yuv.Stride / 2 + x / 2) * 2;
+
+			public byte this[int x, int y]
+			{
+				get => _yuv.Buffer[Index(x, y)];
+				set => _yuv.Buffer[Index(x, y)] = value;
+			}
+		}
+
+		public class VBuffer
+		{
+			private readonly YUV _yuv;
+			private readonly int _start;
+
+			public VBuffer(YUV yuv)
+			{
+				_yuv = yuv;
+				_start = yuv.Stride * yuv.Height;
+			}
+
+			private int Index(int x, int y)
+				=> _start + (y / 2 * _yuv.Stride / 2 + x / 2) * 2 + 1;
+
+			public byte this[int x, int y]
+			{
+				get => _yuv.Buffer[Index(x, y)];
+				set => _yuv.Buffer[Index(x, y)] = value;
+			}
+		}
+	}
+}
