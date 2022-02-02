5eaa8d82e876f1e991780b5c60a720b2e689b412
Samarth Asthana <sasthana@microsoft.com>
2022-02-02 08:39:30 -0800

    Working part one of the deployment with staging files creation

---
+++ Samples/V1.0Samples/LocalMediaSamples/ADF/release-az/Import-UploadWebCert.ps1
@@ -0,0 +1,43 @@
+param (
+    [Parameter(Mandatory)]
+    [String]$Location,
+    [Parameter(Mandatory)]
+    [String]$OrgName,
+    [Parameter(Mandatory)]
+    [String]$KVName
+)
+
+Write-Output "$('-'*50)"
+Write-Output $MyInvocation.MyCommand.Source
+
+# TLS Cert
+Write-Verbose -Message "Primary KV Name:`t $KVName certificate]" -Verbose
+if (! (Get-AzKeyVaultCertificate -VaultName $KVName -Name WildcardCert -EA SilentlyContinue))
+{
+    try
+    {
+        $PfxFilePath = Read-Host -Prompt "Enter the PFX Cert File Path"
+        # trim quotes if the user enters quotes
+        $PfxFilePathTrimmed = $PfxFilePath.Trim('"').Trim("'")
+        
+        if (! (Test-Path -Path $PfxFilePathTrimmed))
+        {
+            Throw "PFX File not found [$PfxFilePathTrimmed]"
+        }
+
+        $PW = Read-Host -AsSecureString -Prompt "Enter the certificate password"
+        Import-AzKeyVaultCertificate -FilePath $PfxFilePathTrimmed -Name WildcardCert -VaultName $KVName -Password $PW -OutVariable kvcert
+
+        $Thumbprint = ConvertTo-SecureString -String $kvcert.Thumbprint -AsPlainText -Force
+        Set-AzKeyVaultSecret -VaultName $KVName -Name 'CertificateThumbprint' -SecretValue $Thumbprint
+    }
+    catch
+    {
+        Write-Warning $_
+        break
+    }
+}
+else
+{
+    Write-Warning "Certificate exists:`t [WildcardCert]"
+}
