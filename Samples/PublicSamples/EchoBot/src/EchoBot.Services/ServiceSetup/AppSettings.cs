6d390e3d94eeb7b34c5e0c8ca468554a2bd731fc
Brennen Cage <brennen.cage@outlook.com>
2022-03-25 13:12:06 -0400

    added comments to app settings

--- Samples/PublicSamples/EchoBot/src/EchoBot.Services/ServiceSetup/AppSettings.cs
+++ Samples/PublicSamples/EchoBot/src/EchoBot.Services/ServiceSetup/AppSettings.cs
@@ -29,13 +29,21 @@
         /// <value>The aad application secret.</value>
         public string AadAppSecret { get; set; }
 
+        /// <summary>
+        /// Internal hosting protocol for the bot
+        /// With 'UseLocalDevSettings' this will be http
+        /// </summary>
         public string BotInternalHostingProtocol = "https";
 
         /// <summary>
-        /// Gets or sets the instance internal port.
+        /// Gets or sets the instance media internal port.
         /// </summary>
         /// <value>The instance internal port.</value>
         public int MediaInternalPort { get; set; }
+
+        /// <summary>
+        /// Gets or sets the instance bot notifications internal port
+        /// </summary>
         public int BotInternalPort { get; set; }
 
         /// <summary>
@@ -46,15 +54,58 @@
         /// <value>The call signaling port.</value>
         public int BotCallingInternalPort { get; set; }
 
+        /// <summary>
+        /// Gets or sets if the bot should use Cognitive Services
+        /// for converting the audio to a Bot voice
+        /// </summary>
         public bool UseCognitiveServices { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Cognitive Services Speech key
+        /// </summary>
         public string SpeechConfigKey { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Cognitive Services Speech region
+        /// </summary>
         public string SpeechConfigRegion { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Cognitive Services Bot language
+        /// that it will use for speech-to-text and text-to-speech
+        /// </summary>
         public string BotLanguage { get; set; }
+
         // set by dsc script
+
+        /// <summary>
+        /// Gets or sets the Load Balancer port for the specific VM instance
+        /// used for call notifications
+        /// </summary>
         public int BotInstanceExternalPort { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Load Balancer port for the specific VM instance
+        /// used for media notifications
+        /// </summary>
         public int MediaInstanceExternalPort { get; set; }
-        public bool UseLocalDevSettings { get; set; }
-        public string MediaDnsName { get; set; }
+        
+        /// <summary>
+        /// Gets or sets the Application Insights Instrumentation key
+        /// used for logging to app insights
+        /// </summary>
         public string AppInsightsInstrumentationKey { get; set; }
+
+        /// <summary>
+        /// Used for local development to set the ports to be used
+        /// with ngrok
+        /// </summary>
+        public bool UseLocalDevSettings { get; set; }
+
+        /// <summary>
+        /// Set by the user only when using local dev settings
+        /// since the media settings needs a different URI
+        /// </summary>
+        public string MediaDnsName { get; set; }
     }
 }
