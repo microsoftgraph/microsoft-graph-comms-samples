fa04b1bd3b0771fe775329a3bdf008dd4ee9bf25
Brennen Cage <brennen.cage@microsoft.com>
2023-09-06 09:07:38 -0400

    added health endpoint and fixed deployment issues

---
+++ Samples/PublicSamples/EchoBot/src/EchoBot.Api/Controllers/HealthController.cs
@@ -0,0 +1,38 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace EchoBot.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HealthController : ControllerBase
+    {
+        private readonly ILogger<HealthController> _logger;
+
+        public HealthController(ILogger<HealthController> logger)
+        {
+            _logger = logger;
+        }
+
+        /// <summary>
+        /// Health check endpoint.
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public IActionResult Health()
+        {
+            try
+            {
+                _logger.LogInformation("HEALTH CALL");
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, $"Received HTTP {this.Request.Method}, {this.Request.Path}");
+
+                return Problem(detail: e.StackTrace, statusCode: (int)HttpStatusCode.InternalServerError, title: e.Message);
+            }
+        }
+    }
+}
