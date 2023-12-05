5278053c2c192f90db08a919e537a8ff09208d8c
Rakulan Subramaniam <Rakulan.Subramaniam@LM-AG.de>
2023-12-05 15:01:26 +0100

    Migrieren von der ASP.NET-Web-API zu ASP.NET Core

--- Samples/PublicSamples/RecordingBot/src/RecordingBot.Services/Http/Controllers/JoinCallController.cs
+++ Samples/PublicSamples/RecordingBot/src/RecordingBot.Services/Http/Controllers/JoinCallController.cs
@@ -11,27 +11,27 @@
 // </copyright>
 // <summary></summary>
 // ***********************************************************************
-using Microsoft.Extensions.Options;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Primitives;
 using Microsoft.Graph;
 using Microsoft.Graph.Communications.Common.Telemetry;
-using Microsoft.Graph.Communications.Core.Serialization;
+using RecordingBot.Model.Extension;
 using RecordingBot.Model.Constants;
 using RecordingBot.Model.Models;
 using RecordingBot.Services.Contract;
 using RecordingBot.Services.ServiceSetup;
 using System;
-using System.Net;
+using System.Linq;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
-using System.Web.Http;
 
 namespace RecordingBot.Services.Http.Controllers
 {
     /// <summary>
     /// JoinCallController is a third-party controller (non-Bot Framework) that can be called in CVI scenario to trigger the bot to join a call.
     /// </summary>
-    public class JoinCallController : ApiController
+    [ApiController]
+    public class JoinCallController : ControllerBase
     {
         /// <summary>
         /// The logger
@@ -52,29 +52,16 @@ namespace RecordingBot.Services.Http.Controllers
 
         /// <summary>
         /// Initializes a new instance of the <see cref="JoinCallController" /> class.
-
-        /// </summary>
-        public JoinCallController()
-        {
-            _logger = AppHost.AppHostInstance.Resolve<IGraphLogger>();
-            _eventPublisher = AppHost.AppHostInstance.Resolve<IEventPublisher>();
-            _botService = AppHost.AppHostInstance.Resolve<IBotService>();
-            _settings = AppHost.AppHostInstance.Resolve<IOptions<AzureSettings>>().Value;
-        }
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="JoinCallController" /> class.
-
         /// </summary>
         /// <param name="logger">The logger.</param>
         /// <param name="eventPublisher">The event publisher.</param>
         /// <param name="botService">The bot service.</param>
         /// <param name="settings">The settings.</param>
-        public JoinCallController(IGraphLogger logger, IEventPublisher eventPublisher, IBotService botService, IAzureSettings settings)
+        public JoinCallController(IGraphLogger logger, IEventPublisher eventPublisher, IBotService botService, AzureSettings azureSettings)
         {
             _logger = logger;
             _botService = botService;
-            _settings = (AzureSettings)settings;
+            _settings = azureSettings;
             _eventPublisher = eventPublisher;
         }
 
@@ -85,7 +72,7 @@ namespace RecordingBot.Services.Http.Controllers
         /// <returns>The <see cref="HttpResponseMessage" />.</returns>
         [HttpPost]
         [Route(HttpRouteConstants.JoinCall)]
-        public async Task<HttpResponseMessage> JoinCallAsync([FromBody] JoinCallBody joinCallBody)
+        public async Task<IActionResult> JoinCallAsync([FromBody] JoinCallBody joinCallBody)
         {
             try
             {
@@ -94,42 +81,29 @@ namespace RecordingBot.Services.Http.Controllers
                 var callUri = $"{_settings.ServiceCname}{callPath}";
                 _eventPublisher.Publish("JoinCall", $"Call.id = {call.Id}");
 
-                var values = new JoinURLResponse()
+                return Ok(new JoinURLResponse
                 {
                     Call = callUri,
                     CallId = call.Id,
                     ScenarioId = call.ScenarioId
-                };
-
-                var serializer = new CommsSerializer(pretty: true);
-                var json = serializer.SerializeObject(values);
-                var response = this.Request.CreateResponse(HttpStatusCode.OK);
-                response.Content = new StringContent(json, Encoding.UTF8, "application/json");
-                return response;
+                });
             }
             catch (ServiceException e)
             {
-                HttpResponseMessage response = (int)e.StatusCode >= 300
-                    ? this.Request.CreateResponse(e.StatusCode)
-                    : this.Request.CreateResponse(HttpStatusCode.InternalServerError);
-
                 if (e.ResponseHeaders != null)
                 {
                     foreach (var responseHeader in e.ResponseHeaders)
                     {
-                        response.Headers.TryAddWithoutValidation(responseHeader.Key, responseHeader.Value);
+                        Response.Headers.Add(responseHeader.Key, new StringValues(responseHeader.Value.ToArray()));
                     }
                 }
 
-                response.Content = new StringContent(e.ToString());
-                return response;
+                return StatusCode((int)e.StatusCode < 300 ? 500 : (int)e.StatusCode, e.ToString());
             }
             catch (Exception e)
             {
-                _logger.Error(e, $"Received HTTP {this.Request.Method}, {this.Request.RequestUri}");
-                HttpResponseMessage response = this.Request.CreateResponse(HttpStatusCode.InternalServerError);
-                response.Content = new StringContent(e.Message);
-                return response;
+                _logger.Error(e, $"Received HTTP {this.Request.Method}, {this.Request.GetUri()}");
+                return StatusCode(500, e.ToString());
             }
         }
     }
