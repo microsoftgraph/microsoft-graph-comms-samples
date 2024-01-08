b46e8b84e60c93cad3afc610bc0f1838193cb8e8
Fabian Kamp <Fabian.Kamp@LM-AG.de>
2024-01-08 13:55:24 +0100

    Add cert-manager installation script

---
+++ Samples/PublicSamples/RecordingBot/deploy/cert-manager/install.bat
@@ -0,0 +1,20 @@
+@echo off
+
+echo Updating helm repo
+helm repo add jetstack https://charts.jetstack.io
+helm repo update
+
+echo Installing cert-manager
+helm upgrade ^
+  cert-manager jetstack/cert-manager ^
+  --namespace cert-manager ^
+  --create-namespace ^
+  --version v1.13.3 ^
+  --install ^
+  --set nodeSelector."kubernetes\.io/os"=linux ^
+  --set webhook.nodeSelector."kubernetes\.io/os"=linux ^
+  --set cainjector.nodeSelector."kubernetes\.io/os"=linux ^
+  --set installCRDs=true
+
+echo Waiting for cert-manager to be ready
+kubectl wait pod -n cert-manager --for condition=ready --timeout=60s --all
