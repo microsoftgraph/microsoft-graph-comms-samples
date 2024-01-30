04f40da883f185b54d1aadc870e09d0f73c07d36
1fabi0 <fabiankamp3@gmail.com>
2024-01-30 13:35:35 +0100

    Remove C#12 Collection Expression

--- Samples/PublicSamples/RecordingBot/src/RecordingBot.Model/Constants/SerializerAssemblies.cs
+++ Samples/PublicSamples/RecordingBot/src/RecordingBot.Model/Constants/SerializerAssemblies.cs
@@ -24,8 +24,8 @@ namespace RecordingBot.Model.Constants
             {
                 if (_distinctAssemblies == null)
                 {
-                    HashSet<Assembly> hashSet = [];
-                    List<Assembly> list = [];
+                    HashSet<Assembly> hashSet = new HashSet<Assembly>();
+                    List<Assembly> list = new List<Assembly>();
                     _assemblies = _assemblies.Where(assembly => assembly != null);
                     foreach (Assembly item in _assemblies)
                     {
