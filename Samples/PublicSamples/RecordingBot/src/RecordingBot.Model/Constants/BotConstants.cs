320b990448324b5508be340eec293d287baf92a2
InDieTasten <indietasten@gmail.com>
2024-03-08 05:29:15 +0000

    Fix build error

--- Samples/PublicSamples/RecordingBot/src/RecordingBot.Model/Constants/BotConstants.cs
+++ Samples/PublicSamples/RecordingBot/src/RecordingBot.Model/Constants/BotConstants.cs
@@ -4,5 +4,6 @@ namespace RecordingBot.Model.Constants
     {
         public const uint NumberOfMultiviewSockets = 3;
         public const string DefaultOutputFolder = "teams-recording-bot";
+        public const string topicEndpoint = "https://{0}.{1}-1.eventgrid.azure.net/api/events";
     }
 }
